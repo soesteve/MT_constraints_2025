db0a0ff9116377c7f70dc7d51b3ed130ae5db882
soesteve <soesteve@ucm.es>
2026-05-10 21:44:11 +0200

    Comments
    Comments

--- Project_MT_c++/GEN_FU_MR8.cpp
+++ Project_MT_c++/GEN_FU_MR8.cpp
@@ -1,13 +1,13 @@
-// Este programa calcula los fu correspondiente a MR5
-// donde se pone a 0 el primer valor de rc
+ï»¿// This program is part of a project that generates follow-up files for the PSPLIB benchmarks, 
+// specifically for the J30 problem set. 
+// The program calculates the follow-ups corresponding to MR8 based on the original .dzn files,
+// where the first value of rc is set to 0
 
 #include <iostream>
 #include <fstream>
 #include <iterator>
 #include <string>
 #include <regex>
-#include <iostream>
-#include <fstream>
 #include <sstream>
 #include <vector>
 #include <algorithm>
@@ -19,226 +19,117 @@ struct tFiles {
 };
 
 
-void procesa(string file_name, string fu_name);
-void procesa_todos(string file_name, string fu_name);
+void proccess(string file_name, string fu_name);
+void proccess_all(string file_name, string fu_name);
 
 
 int main() {
 
-
-	string path_file = "C:\\Users\\Sonia\\Desktop\\MT_constraints_2025\\benchmarks\\data\\data_psplib\\j30\\J30";
-	string path_fu = "C:\\Users\\Sonia\\Desktop\\MT_constraints_2025\\benchmarks\\data\\data_psplib_follow_ups\\MR5\\j30\\J30";
-	procesa_todos(path_file, path_fu);
+	string path_file = "../benchmarks/data/data_psplib/j30/J30";
+	string path_fu = "../benchmarks/data/data_psplib_follow_ups/MR8/j30/J30";
+	proccess_all(path_file, path_fu);
 
 }
 
-void procesa_todos(string path_file, string path_fu) {
-
+void proccess_all(string path_file, string path_fu) {
 	string s = "";
 
 	for (int i = 1; i <= 48; i++) {  // 48
 		for (int j = 1; j <= 10; j++) {  // 10
 			string path_file_aux = path_file + "_" + to_string(i) + "_" + to_string(j);
 			string path_fu_aux = path_fu + "_" + to_string(i) + "_" + to_string(j);
-			procesa(path_file_aux, path_fu_aux);
+			proccess(path_file_aux, path_fu_aux);
 		}
 	}
 }
 
 
-//----------------
-
-// Función auxiliar para extraer números de una línea con formato tipo "[ 12, 13, 4, 12 ]"
-vector<int> extraer_array(const string& linea) {
-    vector<int> resultado;
-    size_t ini = linea.find('[');
-    size_t fin = linea.find(']');
-    if (ini != string::npos && fin != string::npos) {
-        string contenido = linea.substr(ini + 1, fin - ini - 1);
-        stringstream ss(contenido);
-        string numero;
-        while (getline(ss, numero, ',')) {
-            int valor = stoi(numero);
-            resultado.push_back(valor);
-        }
-    }
-    return resultado;
-}
-
-// Función para extraer una fila de la matriz rr
-vector<int> extraer_fila_rr(const string& linea) {
-    vector<int> fila;
-    size_t ini = linea.find('|');
-    if (ini != string::npos) {
-        string contenido = linea.substr(ini + 1);
-        stringstream ss(contenido);
-        string numero;
-        while (getline(ss, numero, ',')) {
-            int valor = stoi(numero);
-            fila.push_back(valor);
-        }
-    }
-    return fila;
-}
-
-void procesa(string file_name, string fu_name) {
-
-    ifstream entrada(file_name + ".dzn");
-    ofstream salida(fu_name + "_fu_1_rc_mayor_rr.dzn");
-
-    string linea;
-    vector<string> lineas_originales;
-    vector<string> rr_lines;
-    vector<int> primera_fila_rr;
-    vector<int> rc;
-
-    // Leer todo el archivo
-    while (getline(entrada, linea)) {
-        lineas_originales.push_back(linea);
-    }
-
-    // Procesar rc
-    for (const string& l : lineas_originales) {
-        if (l.find("rc") != string::npos) {
-            rc = extraer_array(l);
-        }
-        if (l.find("rr") != string::npos) {  // primera línea
-        // Todas las líneas
-        //if (l.find("rr") != string::npos || (!rr_lines.empty() && l.find("|") != string::npos)) {
-            rr_lines.push_back(l);
-        }
-    }
-
-    // Obtener la primera fila completa unida
-    string primera_fila_completa = rr_lines[0];
-    if (primera_fila_completa.find('|') != string::npos && primera_fila_completa.find(']') == string::npos) {
-        for (size_t i = 1; i < rr_lines.size(); ++i) {
-            if (rr_lines[i].find('|') != string::npos) {
-                primera_fila_completa += rr_lines[i];
-            }
-            else {
-                break;
-            }
-        }
-    }
-
-    // Obtener el máximo de la primera fila
-    primera_fila_rr = extraer_fila_rr(primera_fila_completa);
-    int maximo = *max_element(primera_fila_rr.begin(), primera_fila_rr.end());
-
-    // Sustituir el primer valor de rc
-    if (!rc.empty()) {
-        rc[0] = maximo;
-    }
-
-    // Escribir salida
-    for (const string& l : lineas_originales) {
-        if (l.find("rc") != string::npos) {
-            salida << "rc = [ ";
-            for (size_t i = 0; i < rc.size(); ++i) {
-                salida << rc[i];
-                if (i < rc.size() - 1) salida << ", ";
-            }
-            salida << " ];" << endl;
-        }
-        else {
-            salida << l << endl;
-        }
-    }
-
-    entrada.close();
-    salida.close();
-
-}
 
 
-/*
-void procesa(string file_name, string fu_name) {
+void proccess(string file_name, string fu_name) {
 
 	ifstream in(file_name + ".dzn");
-	ofstream out(fu_name + "_fu_1_rc_0.dzn");
+	if (!in) {
+		cerr << "Unable to open file " << file_name + ".dzn" << endl;
+		exit(1);   // call system to stop
+	}
+	ofstream out(fu_name + "_fu_dur_0.dzn");   // MR8
+	if (!out) {
+		cerr << "Unable to open file " << fu_name + "_fu_dur_0.dzn" << endl;
+		exit(1);   // call system to stop
+	}
 
 	std::string s = "";
 
-	// Las líneas se leen y escriben directamente pq no cambian 
-	// hasta encontrar una línea que comienza con rc
+	// The lines are read and written directly because they don't change
+	// until a line that begins with d is found.
 
-	std::string prefix = "rc";
+	std::string prefix = "d";
 
-	// Trabajo con expresiones regulares para quitar = ; [ ]
+	// I work with regular expressions to remove = ; [ ]
 	std::regex word_regex("(\\w+)");
 
-	int num_rc = 0;
+	int num_tasks = 0;
 	std::getline(in, s);
 	while (s.compare(0, prefix.size(), prefix) != 0) {
 
-		// Necesito saber el número de n_res pero está pegado al ; final
+		// I need to know the number of tasks but it's stuck to the final ;
 
-		// Uso iteradores 
+		// iteratos
 		std::sregex_iterator i = std::sregex_iterator(s.begin(), s.end(), word_regex);
 		std::smatch match = *i;
 		std::string match_str = match.str();
 
-		if (match_str.compare("n_res") == 0) {
+		if (match_str.compare("n_tasks") == 0) {
 			++i;
 			match = *i;
 			match_str = match.str();
-			num_rc = stoi(match_str);  // casting de string a int
+			num_tasks = stoi(match_str);  // casting from string to int
 		}
 
 		out << s << endl;
 		std::getline(in, s);
 	}
 
-	// la línea que tengo en s es de la forma rc = [...]
+	// The line I have in s is of the form d = [...]
 
 	std::sregex_iterator i = std::sregex_iterator(s.begin(), s.end(), word_regex);
 	std::smatch match = *i;
 	std::string match_str = match.str();
 
-	string* array_din_rc = new string[num_rc];
+	string* array_din_durations = new string[num_tasks];
 
-	// Guardo los datos rc = [ ... ]; en un array dinámico array_din_rc
-
-	for (int k = 0; k < num_rc; k++) {
+	// I store the data d = [ ... ]; in a dynamic array array_din_durations
+	// and at the same time I calculate the sum
+	for (int k = 0; k < num_tasks; k++) {
 		++i;
 		match = *i;
 		match_str = match.str();
-		array_din_rc[k] = match_str;
+		array_din_durations[k] = match_str;
 	}
 
-	// LAS SIGUIENTES DOS LÍNEAS LAS LEO  sobre string para escribirlas 
-	// más tarde
+	array_din_durations[0] = "0";  // set the first value of rc to 0
 
-	string s1, s2;
-	std::getline(in, s1);  // n_tasks = 30;
-	std::getline(in, s2);  // 	d = [ ... ];
+	// Generate the output
 
+	out << "d = [ ";
 
-	// Genero la salida
-
-	out << "rc = [ 0, ";   // el primer elemento es 0
-
-	for (int k = 1; k < num_rc - 1; k++) {  // copio los demás eementos
-		out << array_din_rc[k];   // añado elemento a elemento
+	for (int k = 0; k < num_tasks - 1; k++) {
+		out << array_din_durations[k];   // add element by element
 		out << ", ";
 	}
 
-	// Escribo el último elemento y cierro
-	out << array_din_rc[num_rc - 1] << " ];\n";
+	// Write the last element and close
+	out << array_din_durations[num_tasks - 1] << " ];\n";
 
+	// The rest of the lines are read and written directly because they don't change
 
-
-	// El resto de líneas se leen y escriben directamente pq no cambian
-
-	std::getline(in, s);
+	getline(in, s);
 	while (in) {
 		out << s + "\n";
-		std::getline(in, s);
+		getline(in, s);
 	}
 
 	out.close();
 	in.close();
 }
-*/
-
